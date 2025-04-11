e4c4198db2b58c4d9dee4e82019c047210ef7c82
Ignacio <ignaciohuichal@gmail.com>
2025-04-11 16:21:49 -0400

    WIP: improve page

--- src/React/SkillsList.tsx
+++ src/React/SkillsList.tsx
@@ -64,10 +64,7 @@ const SkillsList = () => {
   };
 
   return (
-    <div className="text-left pt-3 md:pt-9">
-      <h3 className="text-[var(--white)] text-3xl md:text-4xl font-semibold md:mb-6">
-        Qué ofrecemos
-      </h3>
+    <div className="text-left">
       <ul className="space-y-4 mt-4 text-lg">
         {Object.entries(skills).map(([category, items]) => (
           <li key={category} className="w-full">
